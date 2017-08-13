86701562b8021b84145bba49c283c01f942193e1
胡佳文 <hu@hujiawendeMac-mini.local>
2017-08-13 23:11:22 +0800

    01对象模板模式

---
+++ Swift设计模式/Swift设计模式/01 对象模板模式/01.playground/Contents.swift
@@ -0,0 +1,51 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+
+///模拟商品数据
+var products = [("Ka","A boat for one person",25.0,10),
+                ("Li","Protecctive and fashionable",48.95,14),
+                ("So","FIAFA size and weight",19.5,32)]
+
+/* 以下是错误的设计模式--- 由于对数据的过度依赖index 形成了高耦合 难以进行维护
+    当从模拟商品数据删除某一项数据时 会造成程序的崩溃
+ */
+///计算商品税 20%
+func calculateTax(produce:(String,String,Double,Int)) -> Double {
+    return produce.2 * 0.2
+}
+///计算总价格
+func calculateValue(dataArr:[(String,String,Double,Int)]) -> Double {
+    return dataArr.reduce(0, {
+        (total,product) -> Double in
+        return total + (product.2 * Double(product.3))
+    })
+}
+calculateTax(produce: products[0])
+calculateValue(dataArr: products)
+
+///所以面向对象编程思想能够更好的避免这个问题 将元组数据进行抽象 模块化 
+struct productStr {
+    let name: String
+    let detail: String
+    let price: Double
+    let number: Int
+}
+let proStr = productStr(name: "KA", detail: "st", price: 10.2, number: 10)
+//class
+class productClass {
+    open var name: String //open 最高的开放权限
+    public var detail: String //publish公开的访问权限 internal 为默认权限
+    fileprivate var price: Double //fileprivate 文件内访问权限
+    private var number: Int //private 私有访问权限 
+    // final 关键字可以用在 class，func 或者 var 前面进行修饰，表示不允许对该内容进行继承或者重写操作。
+    init(name: String, detail: String, price: Double, number: Int) {
+        self.name = name
+        self.detail = detail
+        self.price = price
+        self.number = number
+    }
+}
+let proClass = productClass(name: "FA", detail: "fi", price: 20.2, number: 3)
+
---
+++ Swift设计模式/Swift设计模式/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  Swift设计模式
+//
+//  Created by 胡佳文 on 2017/8/13.
+//  Copyright © 2017年 胡佳文. All rights reserved.
+//
+
+import Foundation
